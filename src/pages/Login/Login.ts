6c921b6657f80794b4b82f8f407636adbc1de553
liupei <2771873200@qq.com>
2018-05-07 18:26:59 +0800

    判断逻辑

--- src/pages/Login/Login.ts
+++ src/pages/Login/Login.ts
@@ -2,7 +2,6 @@ import component from 'vue-class-component'
 import Vue from 'vue'
 import VerifyCode from '@/components/VerifyCode/VerifyCode.vue'
 import Http from './lib/http'
-const { Indicator } = require('mint-ui')
 
 @component({
     name: 'login',
@@ -16,15 +15,27 @@ export default class Login extends Vue {
     private password: string = ''
     private VCode: string = ''
     private ICode: string =''
+    private toast: any = undefined
 
     loginSystem() {
-        this.validUserInfo()
-        Indicator.open('加载中...')
-        console.log(1234)
+        this.toast && this.toast.close()
+        if (this.validUserInfo() === 'Allow To Login') {
+            IMint.Indicator.open({
+                text: '登录中...',
+                spinnerType: 'snake'
+            })
+        }
     }
 
     validUserInfo() {
-        return 'asd'
+        if (!this.username.trim()) return (this.toast = (<any>this).$toast('用户名不能为空'))
+        if (!this.password.trim()) return (this.toast = (<any>this).$toast('请输入密码'))
+        if (!this.ICode.trim()) {
+            return (this.toast = (<any>this).$toast('请输入验证码'))
+        } else {
+            if (this.ICode.trim() !== this.VCode) return (this.toast = (<any>this).$toast('验证码输入错误'))
+        }
+        return 'Allow To Login'
     }
 
     getVCode(code: string) {
